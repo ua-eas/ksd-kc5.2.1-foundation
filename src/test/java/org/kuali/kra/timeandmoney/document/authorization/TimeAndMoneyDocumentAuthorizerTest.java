73becb6220436360a2a7350be6a1c461b61478f3
Scott G. Skinner <sskinner@email.arizona.edu>
2014-12-08 13:57:40 -0700

    UAR-417 - Modifiying TimeAndMoneyDocumentAuthorizerTest to use new base class, removing previous role logic.

--- src/test/java/org/kuali/kra/timeandmoney/document/authorization/TimeAndMoneyDocumentAuthorizerTest.java
+++ src/test/java/org/kuali/kra/timeandmoney/document/authorization/TimeAndMoneyDocumentAuthorizerTest.java
@@ -15,11 +15,9 @@
  */
 package org.kuali.kra.timeandmoney.document.authorization;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,173 +27,44 @@ import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.kim.bo.KcKimAttributes;
 import org.kuali.kra.test.infrastructure.PersonAndRoleAwareTestBase;
 import org.kuali.kra.timeandmoney.document.TimeAndMoneyDocument;
-import org.kuali.rice.kim.api.group.Group;
-import org.kuali.rice.kim.api.group.GroupService;
 import org.kuali.rice.kim.api.identity.Person;
-import org.kuali.rice.kim.api.role.Role;
-import org.kuali.rice.kim.api.role.RoleService;
-import org.kuali.rice.kim.impl.role.RoleMemberAttributeDataBo;
-import org.kuali.rice.kim.impl.role.RoleMemberBo;
-import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.service.DocumentService;
 
-@SuppressWarnings({"rawtypes", "unchecked", "deprecation"})
+@SuppressWarnings({"deprecation"})
 public class TimeAndMoneyDocumentAuthorizerTest extends PersonAndRoleAwareTestBase {
     
     private TimeAndMoneyDocument timeAndMoneyDocument;
     private DocumentService documentService;
-    private BusinessObjectService businessObjectService;
     private TimeAndMoneyDocumentAuthorizer authorizer;
     private Person quickstart;
     private Person borst;
-    private Person irbAdmin;
     private Person iacucAdmin;
     
     @Before
     public void setup() throws Exception {
-        documentService = KraServiceLocator.getService(DocumentService.class);
-        businessObjectService = KraServiceLocator.getService(BusinessObjectService.class);
-        timeAndMoneyDocument = (TimeAndMoneyDocument) documentService.getNewDocument(TimeAndMoneyDocument.class);
-        authorizer = new TimeAndMoneyDocumentAuthorizer();
 
         quickstart = createPerson(KimPersonFixture.QUICKSTART);
         borst = createPerson(KimPersonFixture.BORST);
-        irbAdmin = createPerson(KimPersonFixture.IRB_ADMIN);
         iacucAdmin = createPerson(KimPersonFixture.IACUC_ADMIN);
-        
+
         addPersonToRole(quickstart, KimRoleFixture.TIME_AND_MONEY_MODIFIER);
-        addIrbAdminToAGroupWithTimeAndMoneyPerm();
-        addIacucAdminToTimeAndMoneyRole();
+        addPersonToRole(quickstart, KimRoleFixture.AWARD_MODIFIER);
+
+        documentService = KraServiceLocator.getService(DocumentService.class);
+        timeAndMoneyDocument = (TimeAndMoneyDocument) documentService.getNewDocument(TimeAndMoneyDocument.class);
+        authorizer = new TimeAndMoneyDocumentAuthorizer();
     }
     
     @After
     public void teardown() {
         documentService = null;
-        businessObjectService = null;
         timeAndMoneyDocument = null;
         authorizer = null;
         quickstart = null;
         borst = null;
-        irbAdmin = null;
         iacucAdmin = null;
     }
-    
 
-	private void addIrbAdminToAGroupWithTimeAndMoneyPerm() {
-        RoleService rs = KraServiceLocator.getService(RoleService.class);
-        Role timeAndMoneyModifier = rs.getRoleByNamespaceCodeAndName("KC-T", "Time And Money Modifier");
-        
-        GroupService gs = KraServiceLocator.getService(GroupService.class);
-        gs.createGroup(Group.Builder.create("KC-T", "TimeAndMoneyTestGroup", "21").build());
-        
-        /**
-         * Get the group from the database so we have a valid ID
-         */
-        Group timeAndMoneyTestGroup = gs.getGroupByNamespaceCodeAndName("KC-T", "TimeAndMoneyTestGroup");
-        gs.addPrincipalToGroup(irbAdmin.getPrincipalId(), timeAndMoneyTestGroup.getId());
-        
-        String groupId = timeAndMoneyTestGroup.getId();
-        String namespaceCode = timeAndMoneyModifier.getNamespaceCode();
-        String roleName = timeAndMoneyModifier.getName();
-        rs.assignGroupToRole(groupId, namespaceCode, roleName, new HashMap<String, String>());
-        
-        Map fieldValues = new HashMap();
-        fieldValues.put("ROLE_ID", timeAndMoneyModifier.getId());
-        fieldValues.put("MBR_ID", timeAndMoneyTestGroup.getId());
-        Collection roleMembers = businessObjectService.findMatching(RoleMemberBo.class, fieldValues);
-        RoleMemberBo roleMember = (RoleMemberBo) roleMembers.iterator().next();
-        
-        RoleMemberAttributeDataBo attrData = new RoleMemberAttributeDataBo();
-        attrData.setAttributeValue("000001");
-        attrData.setKimAttributeId("47");
-        attrData.setKimTypeId("69");
-        attrData.setAssignedToId(roleMember.getId());
-        
-        RoleMemberAttributeDataBo attrDataTwo = new RoleMemberAttributeDataBo();
-        attrDataTwo.setAttributeValue("Y");
-        attrDataTwo.setKimAttributeId("48");
-        attrDataTwo.setKimTypeId("69");
-        attrDataTwo.setAssignedToId(roleMember.getId());
-        
-        roleMember.getAttributeDetails().add(attrData);
-        roleMember.getAttributeDetails().add(attrDataTwo);
-        businessObjectService.save(roleMember);
-        businessObjectService.save(attrData);
-        businessObjectService.save(attrDataTwo);
-    }
-    
-
-	private void addIacucAdminToTimeAndMoneyRole() {
-        RoleService rs = KraServiceLocator.getService(RoleService.class);
-        Role timeAndMoneyModifier = rs.getRoleByNamespaceCodeAndName("KC-T", "Time And Money Modifier");
-        
-        //GroupService gs = KraServiceLocator.getService(GroupService.class);
-        //gs.createGroup(Group.Builder.create("KC-T", "TimeAndMoneyTestGroup", "21").build());
-        
-        /**
-         * Get the group from the database so we have a valid ID
-         */
-        //Group timeAndMoneyTestGroup = gs.getGroupByNamespaceCodeAndName("KC-T", "TimeAndMoneyTestGroup");
-        //gs.addPrincipalToGroup(irbAdmin.getPrincipalId(), timeAndMoneyTestGroup.getId());
-        
-        //String groupId = timeAndMoneyTestGroup.getId();
-        String namespaceCode = timeAndMoneyModifier.getNamespaceCode();
-        String roleName = timeAndMoneyModifier.getName();
-        //rs.assignGroupToRole(groupId, namespaceCode, roleName, new HashMap<String, String>());
-        rs.assignPrincipalToRole(iacucAdmin.getPrincipalId(), namespaceCode, roleName, new HashMap<String, String>());
-        
-        
-        Map fieldValues = new HashMap();
-        fieldValues.put("ROLE_ID", timeAndMoneyModifier.getId());
-        fieldValues.put("MBR_ID", iacucAdmin.getPrincipalId());
-        Collection roleMembers = businessObjectService.findMatching(RoleMemberBo.class, fieldValues);
-        RoleMemberBo roleMember = (RoleMemberBo) roleMembers.iterator().next();
-        
-        RoleMemberAttributeDataBo attrData = new RoleMemberAttributeDataBo();
-        attrData.setAttributeValue("000001");
-        attrData.setKimAttributeId("47");
-        attrData.setKimTypeId("69");
-        attrData.setAssignedToId(roleMember.getId());
-        
-        RoleMemberAttributeDataBo attrDataTwo = new RoleMemberAttributeDataBo();
-        attrDataTwo.setAttributeValue("Y");
-        attrDataTwo.setKimAttributeId("48");
-        attrDataTwo.setKimTypeId("69");
-        attrDataTwo.setAssignedToId(roleMember.getId());
-        
-        roleMember.getAttributeDetails().add(attrData);
-        roleMember.getAttributeDetails().add(attrDataTwo);
-        businessObjectService.save(roleMember);
-        businessObjectService.save(attrData);
-        businessObjectService.save(attrDataTwo);
-    }
-    
-    @Test
-    public void verifyRoleStuff() {
-        RoleService rs = KraServiceLocator.getService(RoleService.class);
-        Role timeAndMoneyModifier = rs.getRoleByNamespaceCodeAndName("KC-T", "Time And Money Modifier");
-        GroupService gs = KraServiceLocator.getService(GroupService.class);
-        Group timeAndMoneyTestGroup = gs.getGroupByNamespaceCodeAndName("KC-T", "TimeAndMoneyTestGroup");
-        
-        Map fieldValues = new HashMap();
-        fieldValues.put("ROLE_ID", timeAndMoneyModifier.getId());
-        fieldValues.put("MBR_ID", timeAndMoneyTestGroup.getId());
-        Collection roleMembers = businessObjectService.findMatching(RoleMemberBo.class, fieldValues);
-        RoleMemberBo roleMember = (RoleMemberBo) roleMembers.iterator().next();
-        
-        boolean foundUnit = false;
-        boolean foundHierarchFlag = false;
-        for (RoleMemberAttributeDataBo rmb : roleMember.getAttributeDetails()) {
-            if (StringUtils.equalsIgnoreCase(rmb.getAttributeValue(), "000001")) {
-                foundUnit = true;
-            } else if(StringUtils.equalsIgnoreCase(rmb.getAttributeValue(), "Y")){
-                foundHierarchFlag = true;
-            }
-        }
-        assertEquals(2, roleMember.getAttributeDetails().size());
-        assertTrue(foundUnit);
-        assertTrue(foundHierarchFlag);
-    }
 
     @Test
     public void testAddRoleQualificationObjectMapOfStringString() {
@@ -228,29 +97,5 @@ public class TimeAndMoneyDocumentAuthorizerTest extends PersonAndRoleAwareTestBa
         boolean canQuickstart = authorizer.canClose(timeAndMoneyDocument, quickstart);
         assertTrue(canQuickstart);
     }
-    
-    @Test
-    public void testQuickStartPerm() {
-        RoleService rs = KraServiceLocator.getService(RoleService.class);
-        Role timeAndMoneyModifier = rs.getRoleByNamespaceCodeAndName("KC-T", "Time And Money Modifier");
-        
-        Map fieldValues = new HashMap();
-        fieldValues.put("ROLE_ID", timeAndMoneyModifier.getId());
-        fieldValues.put("MBR_ID", quickstart.getPrincipalId());
-        Collection roleMembers = businessObjectService.findMatching(RoleMemberBo.class, fieldValues);
-        RoleMemberBo roleMember = (RoleMemberBo) roleMembers.iterator().next();
 
-        boolean foundUnit = false;
-        boolean foundHierarchFlag = false;
-        for (RoleMemberAttributeDataBo rmb : roleMember.getAttributeDetails()) {
-            if (StringUtils.equalsIgnoreCase(rmb.getAttributeValue(), "000001")) {
-                foundUnit = true;
-            } else if(StringUtils.equalsIgnoreCase(rmb.getAttributeValue(), "Y")){
-                foundHierarchFlag = true;
-            }
-        }
-        assertEquals(2, roleMember.getAttributeDetails().size());
-        assertTrue(foundUnit);
-        assertTrue(foundHierarchFlag);
-    }
 }
