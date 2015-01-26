14cd4ddddb1b12983060a17b2056ff81a67003d2
Scott G. Skinner <sskinner@email.arizona.edu>
2015-01-26 16:10:07 -0700

    UAR-417 - Changing principal names to be all lower case, because if you do not, rice will, and your person search will come back null.

--- src/test/java/org/kuali/kra/test/fixtures/PersonFixture.java
+++ src/test/java/org/kuali/kra/test/fixtures/PersonFixture.java
@@ -7,11 +7,11 @@ public enum PersonFixture {
     	QUICKSTART("quickstart", "1201", "10000000001"),
     	JTESTER("jtester", "1202", "10000000002"),
     	WOODS("woods", "1203", "10000000003"),
-    	OPS_ADMIN("opsAdmin", "1204", "10000000004"),
+    	OPS_ADMIN("opsadmin", "1204", "10000000004"),
     	NEGOTIATOR("negotiator", "1205", "10000000005"),
     	BORST("borst", "1206", "10000000006"),
-    	IRB_ADMIN("irbAdmin", "1207", "10000000007"),
-    	IACUC_ADMIN("iacucAdmin", "1208", "10000000008"),
+    	IRB_ADMIN("irbadmin", "1207", "10000000007"),
+    	IACUC_ADMIN("iacucadmin", "1208", "10000000008"),
     	CHEW("chew", "1209", "10000000009"),
     	MAJORS("majors", "1210", "10000000010");
     	
