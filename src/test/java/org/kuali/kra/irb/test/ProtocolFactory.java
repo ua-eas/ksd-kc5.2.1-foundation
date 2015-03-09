2380827df114d19f857b7b39df090415fb7f3f64
Adam Kost <akost@eaglecrk.com>
2015-03-09 16:39:55 -0500

    UAR-1142 ProtocolGrantExemptionServiceTest and ProtocolFactory fixed (used existing fixtures)

--- src/test/java/org/kuali/kra/irb/test/ProtocolFactory.java
+++ src/test/java/org/kuali/kra/irb/test/ProtocolFactory.java
@@ -25,8 +25,10 @@ import org.kuali.kra.irb.ProtocolDocument;
 import org.kuali.kra.irb.personnel.ProtocolPerson;
 import org.kuali.kra.irb.personnel.ProtocolUnit;
 import org.kuali.kra.service.KraAuthorizationService;
+import org.kuali.kra.test.fixtures.PersonFixture;
 import org.kuali.kra.test.fixtures.RoleFixture;
 import org.kuali.kra.test.fixtures.UnitFixture;
+import org.kuali.kra.test.helpers.UnitTestHelper;
 import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.krad.service.DocumentService;
 import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
@@ -42,8 +44,8 @@ public class ProtocolFactory {
     private static final String PROTOCOL_NUMBER = "0906000001";
     private static final String PROTOCOL_TYPE_CODE_STR = "1";//test of option "Standard";
     private static final String PROTOCOL_TITLE_STR = "New protocol test";
-    private static final String PRINCIPAL_INVESTIGATOR_ID = "10000000001";
-    private static final String PRINCIPAL_INVESTIGATOR_NAME = "Terry Durkin";
+    private static final String PRINCIPAL_INVESTIGATOR_ID = PersonFixture.JTESTER.getPrincipalId();
+    private static final String PRINCIPAL_INVESTIGATOR_NAME = PersonFixture.JTESTER.getPrincipalName();
     private static final String PRINCIPAL_INVESTIGATOR_UNIT = UnitFixture.TEST_1.getUnitNumber();
     private static final String PRINCIPAL_INVESTIGATOR_ROLE = RoleFixture.PROTOCOL_PI.getRoleName();
     private static final String REFERENCE_PERSON_ROLE = "protocolPersonRole";
@@ -100,6 +102,8 @@ public class ProtocolFactory {
      * @param document
      */
     public static void setProtocolRequiredFields(ProtocolDocument document, String protocolNumber) {
+        UnitTestHelper helper = new UnitTestHelper();
+        helper.createUnit( UnitFixture.TEST_1 );
         Protocol protocol = document.getProtocol();
         document.getDocumentHeader().setDocumentDescription(DEFAULT_DOCUMENT_DESCRIPTION);
         document.setDocumentNextvalues(new ArrayList<DocumentNextvalue>());
